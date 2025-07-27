03c98cbd3c26baa54d00b2dad548a85b5e62509a
Sander Senhorst <s.senhorst@gmail.com>
2025-07-27 21:47:08 +0200

    Changed recursive search to glob

--- src/helpers/find-files-with-extension-recursively.ts
+++ src/helpers/find-files-with-extension-recursively.ts
@@ -1,16 +1,9 @@
-import { extname } from 'path';
-import { getAllFilesRecursively } from './get-all-files-recursively';
+import { glob } from 'glob'
+import { join } from 'path'
 
 export async function findFilesWithExtensionRecursively(dirToSearch: string, extensionsToInclude: string[]): Promise<string[]> {
-  const allFiles = await getAllFilesRecursively(dirToSearch);
-  const dirIsEmpty = allFiles.length === 0;
-  if (dirIsEmpty) {
-    throw new Error('The search directory is empty, so there is no work to do. Check that your --inputDir contains all of the Google Takeout data, and that any zips have been extracted before running this tool');
-  }
-
-  const matchingFiles = allFiles.filter(filePath => {
-    const extension = extname(filePath).toLowerCase();
-    return extensionsToInclude.map(ext => ext.toLowerCase()).includes(extension.toLowerCase());
-  });
+  const extensionString = extensionsToInclude.length > 1 ? `{${extensionsToInclude}}` : `${extensionsToInclude}`
+  const globPattern = join(dirToSearch, '**\\*'+ extensionString);
+  const matchingFiles = glob(globPattern, {windowsPathsNoEscape:true})
   return matchingFiles;
 }
